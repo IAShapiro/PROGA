6d313329fa96094ad541bf29ce1d2d14fabc5cdb
Igor <fpm.shapiro@bsu.by>
2019-03-14 22:36:36 +0300

    5

---
+++ 2LabConcerts/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <windows.h>
+#include "ClassConcert.h"
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Concert A(1);
+	std::ifstream fin("1.txt");
+	while(!fin.eof())
+	{
+		fin >> A;
+	}
+
+	std::cout << A;
+	A.sortName();
+
+	std::cout << A;
+	std::cout << A.getN() << std::endl;
+	std::cout << A[2].name << std::endl;
+	std::cout << A;
+	A.book(3);
+	std::cout << A;
+	system("pause");
+	fin.close();
+	return 0;
+}
